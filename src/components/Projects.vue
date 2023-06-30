cd2958208a7b6acf9948a474f8a3fd4a3594d487
Giulio <giulio.rebuffo@gmail.com>
2023-06-30 20:20:39 +0200

    adds ups

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -23,16 +23,16 @@
         <br>
         When I was still a Junior in the Erigon team, I was tasked with developing "The Merge", which is pretty much
         the Ethereum PoS implementation for the Execution Layer. It was mostly a solo work, I worked with a Senior and another Junior
-        but I ended up doing most of the work. So, the skills I learnt were pretty much management of highly of stress, responsability management.
+        but I ended up doing most of the work. So, the skills I learnt were pretty much management of high levels of stress, responsability management.
         It was mostly a test of stress and patience. 
         <br><br>
         These are the things I have done from a technical standpoint, and what I learnt from this experience:
         <br>
         <ul>
             <li>Resurrected Erigon transaction pool (it did not work before): Learnt how Ethereum transaction pool works.</li>
-            <li>Engine API Implementation: I learnt how consensus-based distributed system works from an archittectural standpoint.</li>
-            <li>Implemented Management system for different EL states: Learnt how execution of transactions works and how to CoW(copy on write) etherum states</li>
-            <li>Learnt how to work with people with different backgrounds(<b>but not incompetent</b>): I worked with different client teams so I worked with people with very diverse world view than mine(ex-Googlers, ex-Ivy students, and more)</li> 
+            <li>Engine API Implementation: I learnt how consensus-based distributed systems works from an archittectural standpoint.</li>
+            <li>Implemented Management system for different EL states: Learnt how execution of transactions works and how to CoW(copy on write) ethereum states</li>
+            <li>Learnt how to work with people with different backgrounds(<b>but not incompetent</b>): I worked with different client teams so I worked with people with very diverse worldviews than mine(ex-Googlers, ex-Ivy students, and more)</li> 
         </ul>
     
     <br>
@@ -40,19 +40,19 @@
     <h3>Developed an entire Consensus Client by myself because why not? </h3>
     <br>
     <br>
-    I decided to create an entire Consensus Layer for the Erigon project because I did not like how other Consensus Layer operates,
-    at the beggining it was just a Consensus Lightclients but there were not enough lightclient servers on Ethereum mainnet so I gave up and went for the full thing.
-    Most of the work was done by myself, because needing 10 developers for a Consensus Layer (like most consensus layers do) is a <b>Skill issue</b> (this is irony). the Consensus Layer, (at least the first prototype) was completed over the course of 9 months I finished it (including the time it took me for the lightclient).
-    At the time of the writting, I am the Project lead (I have not been fired yet :D) and I brought another Company man-power to help so I also have sufficient social skills to
+    I decided to create an entire Consensus Layer for the Erigon project because I did not like how other Consensus Layers operates,
+    at the beggining it was just a Consensus LightClient but there were not enough LightClient servers running on Ethereum mainnet so I gave up and went for the full thing.
+    Most of the work was done by myself, because needing 10 developers for a Consensus Layer (like most consensus layers do) is a <b>Skill issue if you ask me</b> (this is irony). the Consensus Layer, (at least the first prototype) was completed over the course of 9 months (including the time it took me for the lightclient).
+    At the time of the writting, I am the Project Lead (I have not been fired yet :D) and I brought another Company's man-power to help so I also have sufficient social skills to
     bring meaningful partnerships, whether you like it or not.
     <br><br>
     These are the things I have done from a technical standpoint, and what I learnt from this experience:
     <br>
     <ul>
         <li>Learnt inner working of Ethereum 2.0 consensus: Learnt how Ethereum Consensus works.</li>
-        <li>Memeory management for distributed systems: Learnt how to manage and cache data efficiently in distributed system.</li>
-        <li>Efficient Code: Learnt how to optimize CPU.</li>
-        <li>DevOps Skills: I learnt devops from a colleague which wants to stay anon. most notably: Kubernetes.</li> 
+        <li>Memory management for distributed systems: Learnt how to manage and cache data efficiently in distributed systems.</li>
+        <li>Profiling: Learnt how to optimize CPU and Heap memory using profilers.</li>
+        <li>DevOps Skills: I learnt basic devops skills from a colleague which wants to stay anon. most notably: Kubernetes.</li> 
     </ul>
 
     <br>
@@ -60,11 +60,11 @@
     <h3>Robotics oriented research at University of Genoa. </h3>
     <br>
     <br>
-    When I was 14 years old, I became an unpaid slave of the University of Genoa, which is in Italy. I did verious works in the fields of robotic.
-    I was able to use speech recognition to control a robotic arm. I also was able to generate a path finding algorithm for drone path recognizition.
+    When I was 14 years old, I became an unpaid slave at the University of Genoa, which is in Italy. I did various works in the fields of robotic.
+    I was able to use speech recognition to control a robotic arm. I also was able to generate a path finding algorithm for drone path recognition.
     I also worked with a framework called ROS, so I have some experience with math concepts from that world. I also contributed to embedded system research.
-    I also used the Unity engine for simulating robotic movement (although not that proficient using it). I decided that Academia was not my vocation
-    because it underpay you.
+    I also used the Unity engine for simulating the drones movement (although I am not that proficient using it). I decided that Academia was not my vocation
+    because it underpays you.
     <br><br>
     These are the things I have done from a technical standpoint, and what I learnt from this experience:
     <br>
@@ -80,14 +80,14 @@
     <br>
     <br>
     I worked at an Asset-Bridging firm in Switzerland(L0). I built part of their new version for the Backend (in Rust), refactored their code
-    and Implemented calldata passing to their solidity smart contracts. I fired myself from this position. I reviewed/audited some of their Solidity
-    smart contracts and built extensively in Rust onb their Backend.
+    and Implemented calldata passing to their solidity smart contracts. Lastly, I reviewed/audited some of their Solidity
+    smart contracts and built extensively in Rust on their Backend.
     <br><br>
     These are the things I have done from a technical standpoint, and what I learnt from this experience:
     <br>
     <ul>
         <li>Rust: First time I used Rust.</li>
-        <li>Code refactoring: That thing had more duplications than there are in the wild life near Nagasaki.</li>
+        <li>Code refactoring: That thing had more duplications than there are in the genetic code of wildlife near Nagasaki.</li>
         <li>Money Management: They had a lot of money from customers.</li>
     </ul>
 
@@ -98,25 +98,21 @@
     <br>
     <br>
         I implemented the JSON-RPC interface in Akula before it got closed. I implemented the jsonrpc interface according to the Ethereum specs
-        and a an easy way to implement new methods in it. in other words, I implemented in Rust the Ethereum Application-Network interface as it is describe <body>
-        Ethereum documentation. This means I have experience with Ethereum underlying json APIs.
+        and introduced an easy way to implement new methods in it. This means I have experience with Ethereum underlying json APIs.
 
         <ul>
             <li>I know how Ethereum jsonrpc API works: I built them in Rust.</li>
             <li>More proficiency in Rust: I am more proficient in Rust.</li>
         </ul>
-        </body>
     <br>
     <br>
 
 
-    <h3>Protyped and Created Pre-sorting of data before database insertion framework in Go, Rust and C++ </h3>
+    <h3>Protyped and Implemented Pre-sorting of data before database insertion framework in Go, Rust and C++ </h3>
     <br>
     <br>
-    I developed a system that inserts data in sequential order given data in random order of efficient insertion in B-Tree
-    based Databases. Simple implementation of External merge sorting with an heap, not much to stay but it improved writting overhead <body>
-    2x in our databases.
-    </body>
+    I implemented a system that inserts data in sequential order given data in random order of efficient insertion in B-Tree
+    based Databases. Simple implementation of External merge sorting with an heap, not much to stay but it improved writting overhead in our databases.
     <br><br>
     These are the things I have done from a technical standpoint, and what I learnt from this experience:
     <br>
