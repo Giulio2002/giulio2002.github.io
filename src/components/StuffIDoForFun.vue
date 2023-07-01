8564f91a4f1714bc373508505e6b5fa099e58f33
Giulio <giulio.rebuffo@gmail.com>
2023-07-02 00:48:30 +0200

    save

--- src/components/StuffIDoForFun.vue
+++ src/components/StuffIDoForFun.vue
@@ -20,12 +20,15 @@
         <h3>Bullshit #1</h3>
         <br>
         <br>
-        <h3> Open Source Contribution </h3>
+        <h3> Open Source Contributions </h3>
         <br>
         <ul>
+            <li><a href="https://github.com/ledgerwatch/erigon/commits?author=Giulio2002">Erigon</a>: Author of the Caplin Project, implemented Ethereum PoS, and much more...</li>
+            <li><a href="https://github.com/torquem-ch/silkworm">Silkworm</a>: Implemented many of the currently existing syncing component and took care of ETL framework integration.</li>
+            <li><a href="https://github.com/sigp/lighthouse/commits?author=Giulio2002">Akula</a>: Partial implementation LightClient server in Lighthouse.</li>
+            <li><a href="https://github.com/sigp/lighthouse/commits?author=Giulio2002">Lighthouse</a>: Partial implementation LightClient server in Lighthouse.</li>
             <li><a href="https://github.com/UniLogin/UniLogin/commit/8314ceefaf79c8d667ec4c270b9f30cf3397e273">Universal Login</a>: Randomly fixed a major vulnerability in their backend.</li>
             <li><a href="https://github.com/raiden-network/raiden/commit/4431d0625383fead13069abd7186799911868014">Raiden Network</a>: They did not check if an account had enough money to make a transaction... good lord</li>
-            <li><a href="https://github.com/raiden-network/raiden/commit/4431d0625383fead13069abd7186799911868014">Universal Login</a>: They did not check if an account had enough money to make a transaction... good lord</li>
         </ul>
 
     </div>
