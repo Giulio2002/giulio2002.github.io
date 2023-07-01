cdf85aed63e7342000ff2933cb8c88e493e2c87b
Giulio <giulio.rebuffo@gmail.com>
2023-07-01 02:05:35 +0200

    responsive

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -18,23 +18,24 @@ function route(path: String) {
 </script>
 
 <template>
-<MDBNavbar  expand="md" dark >
+<MDBNavbar  expand="md" dark class="header">
+
     <MDBNavbarToggler
         target="#navbarExample03"
         @click="collapse3 = !collapse3"
       ></MDBNavbarToggler>
       <MDBCollapse id="#navbarExample03" v-model="collapse3">
-        <MDBNavbarNav >
-          <MDBNavbarItem href="/"> /home </MDBNavbarItem>
-          <MDBNavbarItem href="/feats"> /feats </MDBNavbarItem>
-          <MDBNavbarItem href="/hackathons"> /hackathons </MDBNavbarItem>
-          <MDBNavbarItem href="/stuffidoforfun"> /stuffidoforfun </MDBNavbarItem>
-          <MDBNavbarItem href="/curriculum"> /curriculum </MDBNavbarItem>
-          <MDBNavbarItem class="link" href="https://substack.com/inbox"> /blog </MDBNavbarItem>
-          <MDBNavbarItem class="link" href="https://github.com/Giulio2002"> /github </MDBNavbarItem>
-          <MDBNavbarItem class="link" href="https://twitter.com/GiulioRebuffo"> /twitter </MDBNavbarItem>
-          <MDBNavbarItem class="link" href="https://myanimelist.net/profile/GiulioRebuffo"> /myanimelist </MDBNavbarItem>
-          <MDBNavbarItem class="link" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"> /inspiration </MDBNavbarItem>
+        <MDBNavbarNav>
+          <MDBNavbarItem  class="item" href="/"> / </MDBNavbarItem>
+          <MDBNavbarItem class="item" href="/feats"> /feats </MDBNavbarItem>
+          <MDBNavbarItem class="item" href="/hackathons"> /hackathons </MDBNavbarItem>
+          <MDBNavbarItem class="item" href="/stuffidoforfun"> /stuffidoforfun </MDBNavbarItem>
+          <MDBNavbarItem class="item" href="/curriculum"> /curriculum </MDBNavbarItem>
+          <MDBNavbarItem class="link item" href="https://substack.com/inbox"> /blog </MDBNavbarItem>
+          <MDBNavbarItem class="link item" href="https://github.com/Giulio2002"> /github </MDBNavbarItem>
+          <MDBNavbarItem class="link item" href="https://twitter.com/GiulioRebuffo"> /twitter </MDBNavbarItem>
+          <MDBNavbarItem class="link item" href="https://myanimelist.net/profile/GiulioRebuffo"> /myanimelist </MDBNavbarItem>
+          <MDBNavbarItem class="link item" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"> /inspiration </MDBNavbarItem>
         </MDBNavbarNav>
       </MDBCollapse>
     </MDBNavbar>
@@ -42,5 +43,16 @@ function route(path: String) {
 <style>
 .link {
     text-decoration: underline;
+}
+
+@media all and (max-width: 768px) {
+    .header {
+        position: absolute;
+    }
+}
+
+
+.item {
+    margin-left: 5px;
 }
 </style>
